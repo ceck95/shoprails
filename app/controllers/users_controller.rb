787a8acfeaec2b16091d4b24175e946169ae9880
nhutuit <tranvannhut4495@gmail.com>
2016-03-06 19:51:19 +0700

    skip_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :authorize
-  before_action :is_admin
+  skip_before_action :is_admin
   # GET /users
   # GET /users.json
   def index
