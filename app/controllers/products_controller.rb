d2b99e6e5e004c40b56d7eda0e42dbd6ba2abc16
nhutuit <tranvannhut4495@gmail.com>
2016-03-09 13:35:06 +0700

    full function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :is_admin
+  before_action :is_admin,only: [:edit, :update, :destroy]
+  skip_before_action :authorize, only:[:show]
   # GET /products
   # GET /products.json
   def index
