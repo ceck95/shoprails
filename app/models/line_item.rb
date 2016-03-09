c0af94b66e4354ad69941ba88e786ac625ce5381
nhutuit <tranvannhut4495@gmail.com>
2016-03-09 12:31:09 +0700

    full function

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,6 @@
 class LineItem < ActiveRecord::Base
   validates :quantity, :presence => { message:"Không được bỏ trống" }
+  validates :quantity, numericality: {message:"Không phải số",greater_than_or_equal_to: 1}
   belongs_to :order
   belongs_to :product
   belongs_to :cart
