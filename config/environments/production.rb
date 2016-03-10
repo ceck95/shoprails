a518148305dd1965b0f4d570a2f79547f15a740b
nhutuit <tranvannhut4495@gmail.com>
2016-03-10 12:12:40 +0700

    full

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-  config.consider_all_requests_local = false
+  # config.consider_all_requests_local = false
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
   # Use a different logger for distributed setups.
