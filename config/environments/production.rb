98d36e91d002bac9bd67a0c142c77072c9668862
nhutuit <tranvannhut4495@gmail.com>
2016-03-10 11:40:42 +0700

    full

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,6 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
-
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
@@ -62,8 +61,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:
+    "smtp.gmail.com",
+    port: 587,
+    domain: "vpsuit.xyz",
+    authentication: "plain",
+    user_name: "tranvannhut4495@gmail.com",
+    password: "nhutnhut",
+    enable_starttls_auto: true
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
